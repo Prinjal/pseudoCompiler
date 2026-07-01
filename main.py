50b1af17beee39b97b680cf065a58a02ec757138
Prinjal <026a218@sxc.edu.np>
2026-07-01 22:59:52 +0545

    token

---
+++ main.py
@@ -0,0 +1,17 @@
+INTEGER , PLUS , EOF = 'INTEGER','PLUS','EOF'
+
+class Token(object):
+    
+    def __init__(self,type,value):
+        self.type = type
+        self.value = value 
+
+    def __str__(self):
+        
+        return 'Token({type},{value})'.format(
+            type = self.type,
+            value = repr(self.value)
+        )
+    
+
+    
