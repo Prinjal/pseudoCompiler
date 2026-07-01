86a33abe685aee304981725bc472f6b2b1435ab1
Prinjal <026a218@sxc.edu.np>
2026-07-01 23:45:16 +0545

    lexer done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-INTEGER , PLUS , EOF = 'INTEGER','PLUS','EOF'
+INTEGER , PLUS , EOF , MINUS = 'INTEGER','PLUS','EOF' , 'MINUS'
 
 class Token(object):
     
@@ -14,5 +14,70 @@ class Token(object):
         )
     
 
-class Interpreter():
+class Lexer():
+
+    def __init__(self,text):
+        
+        self.text = text
+        self.pos = 0
+
+        self.current_char = self.text[self.pos] if len(self.text) > 0 else None
+    def error(self):
+        raise Exception('Error parsing input')
     
+    def advance(self):
+
+        self.pos += 1
+       
+
+        if self.pos > len(self.text) -1:
+            self.current_char =  None
+        else:
+
+            self.current_char = self.text[self.pos]
+
+    
+    def skip_whitespace(self):
+        
+        while self.current_char and self.current_char.isspace() :
+            self.advance()
+        
+    def integer(self):
+        result = ""
+
+        while self.current_char and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            elif self.current_char.isdigit():    
+                number = self.integer()
+                return Token('INTEGER',number)
+
+            elif self.current_char == "+":
+                self.advance()
+                return Token("PLUS",'+')
+            
+            elif self.current_char == "-":
+                self.advance()
+                return Token("MINUS",'-')
+            
+            else:
+                self.error()
+
+        return Token(EOF, None)
+    
+lexer = Lexer("3 + 4 - 2")
+while True:
+    tok = lexer.get_next_token()
+    print(tok)
+    if tok.type == EOF:
+        break
